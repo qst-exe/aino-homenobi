f0d0280f491ad27cac5d80c12f6aa35a596ac013
Task Nagashige <task.19.xx@gmail.com>
2022-12-10 16:29:15 +0900

    :sparkles: OpenAIにリクエストを送るエンドポイントを作成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,28 @@
 import * as functions from "firebase-functions";
+import {Configuration, OpenAIApi} from "openai";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const OPEN_API_KEY = process.env.OPEN_API_KEY;
 
-export const postMessage = functions.https.onCall((data) => {
-  functions.logger.info(data, {structuredData: true});
-  return {reply: "ありがとう"};
+const configuration = new Configuration({
+  apiKey: OPEN_API_KEY,
+});
+
+const formatMessage = (message: string): string => {
+  const questionList = [
+    `「${message}」と言っている人を褒めてください。`,
+    `「${message}」と言っている人を慰めてください。`,
+    `「${message}」と言っている人を元気づけてください。`,
+  ];
+  return questionList[Math.floor(Math.random() * questionList.length)];
+};
+
+export const postMessage = functions.https.onCall(async (data) => {
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: formatMessage(data.message),
+    max_tokens: 500,
+  });
+  return {reply: completion.data.choices[0].text};
 });
